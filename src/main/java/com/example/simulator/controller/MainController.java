525e983c7d18578aaf371c3a3664ee5a50c95efc
Juliusz <54106399+julek64@users.noreply.github.com>
2023-10-09 07:16:18 +0000

    feat: add current measurements endpoint (#2)

--- src/main/java/com/example/simulator/controller/MainController.java
+++ src/main/java/com/example/simulator/controller/MainController.java
@@ -1,13 +1,20 @@
 package com.example.simulator.controller;
 
+import com.example.simulator.simulation.SimulatorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController("/main")
-@RestController
+@RestController()
+@RequestMapping("/api")
 public class MainController {
-    /*@GetMapping("/getAll")
-    public Object getAll() {
 
-    }*/
+    @Autowired
+    private SimulatorService simulatorService;
+
+    @GetMapping(path = "/current-measurements"/*, produces = "application/json"*/)
+    public String getCurrentMeasurements() {
+        return simulatorService.getCurrentMeasurements(0).toString();   // tu zamiast toString trzeba będzie potem zrobić żeby samo się serializowało
+    }
 }
