7f9fdce4a95771191f94e0ecf9984c03f96459d3
Rafał Zaborowski <raffik2000@gmail.com>
2023-11-08 00:37:23 +0100

    describe endpoints

--- src/main/java/com/example/simulator/controller/GreenhouseController.java
+++ src/main/java/com/example/simulator/controller/GreenhouseController.java
@@ -5,6 +5,9 @@ import com.example.simulator.DTOs.SensorValueDTO;
 import com.example.simulator.configurations.response.ResponseData;
 import com.example.simulator.greenhouse.simulators.SimulationType;
 import com.example.simulator.service.GreenhouseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,24 +21,49 @@ public class GreenhouseController {
     @Autowired
     private GreenhouseService greenhouseService;
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201"),
+            @ApiResponse(responseCode = "400", description = "SIMULATION_ALREADY_ADDED"),
+            @ApiResponse(responseCode = "404", description = "GREENHOUSE_DOES_NOT_EXIST")
+    })
+    @Operation(summary = "Add the given simulation type at the specified greenhouse")
     @PostMapping(path = "/addSimulation", produces = "application/json")
     public ResponseEntity<ResponseData> addSimulation(@PathVariable Integer greenhouseId, @RequestParam SimulationType type) {
         greenhouseService.addSimulation(greenhouseId, type);
         return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseData<>());
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", description = "SIMULATION_OF_GIVEN_TYPE_DOES_NOT_EXIST"),
+            @ApiResponse(responseCode = "404", description = "GREENHOUSE_DOES_NOT_EXIST")
+
+    })
+    @Operation(summary = "Delete the given simulation type at the specified greenhouse")
     @DeleteMapping(path = "/deleteSimulation", produces = "application/json")
     public ResponseEntity<ResponseData> deleteSimulation(@PathVariable Integer greenhouseId, @RequestParam SimulationType type) {
         greenhouseService.deleteSimulation(greenhouseId, type);
         return ResponseEntity.status(HttpStatus.OK).body(new ResponseData<>());
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", description = "SIMULATION_OF_GIVEN_TYPE_DOES_NOT_EXIST"),
+            @ApiResponse(responseCode = "404", description = "GREENHOUSE_DOES_NOT_EXIST")
+    })
+    @Operation(summary = "Gives the current value of the given simulation")
     @GetMapping(path = "/getSimulationValue", produces = "application/json")
     public ResponseEntity<ResponseData<SensorValueDTO>> getSimulationValue(@PathVariable Integer greenhouseId, @RequestParam SimulationType type) {
         SensorValueDTO data = greenhouseService.getSimulationValue(greenhouseId, type);
         return ResponseEntity.status(HttpStatus.OK).body(new ResponseData<>(data));
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", description = "SIMULATION_OF_GIVEN_TYPE_DOES_NOT_EXIST"),
+            @ApiResponse(responseCode = "404", description = "GREENHOUSE_DOES_NOT_EXIST")
+    })
+    @Operation(summary = "It is used to manage the temperature in the greenhouse")
     @PutMapping(path = "/setAirConditioningValue", produces = "application/json")
     public ResponseEntity<ResponseData> setAirConditioningValue(@PathVariable Integer greenhouseId, @Validated @RequestBody AirConditioningDTO airConditioningDTO) {
         greenhouseService.setAirConditioningValue(greenhouseId, airConditioningDTO);
