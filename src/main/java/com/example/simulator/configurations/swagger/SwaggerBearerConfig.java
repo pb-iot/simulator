aa1accd402bd359ffdfc38da5114fd2a1d064a2c
Rafał Zaborowski <raffik2000@gmail.com>
2023-11-08 00:21:52 +0100

    fix config swagger

--- src/main/java/com/example/simulator/configurations/swagger/SwaggerBearerConfig.java
+++ src/main/java/com/example/simulator/configurations/swagger/SwaggerBearerConfig.java
@@ -1,13 +1,18 @@
 package com.example.simulator.configurations.swagger;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@OpenAPIDefinition
 public class SwaggerBearerConfig {
     private static final String SCHEME_NAME = "bearerAuth";
     private static final String SCHEME = "bearer";
@@ -15,12 +20,14 @@ public class SwaggerBearerConfig {
     @Bean
     OpenAPI customOpenApi(
             @Value("${simulator.swagger.server.url}") String url,
+            @Value("${simulator.swagger.server.local-url}") String localUrl,
             @Value("${simulator.swagger.info.title}") String title,
             @Value("${simulator.swagger.info.description}") String description,
             @Value("${simulator.swagger.info.version}") String version
     ) {
         return new OpenAPI()
-//                .addServersItem(new Server().url(url))
+                .addServersItem(new Server().url(url))
+                .addServersItem(new Server().url(localUrl))
                 .info(new Info()
                         .title(title)
                         .description(description)
