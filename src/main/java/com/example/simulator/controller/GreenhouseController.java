dbaa5f448d15baaa3f9b0229abef9e1e2651e7ba
Jaaaaaxi <hubert.wasniewskioo@gmail.com>
2024-01-03 13:22:54 +0100

    Refactor GreenhouseController

--- src/main/java/com/example/simulator/controller/GreenhouseController.java
+++ src/main/java/com/example/simulator/controller/GreenhouseController.java
@@ -1,33 +1,19 @@
 package com.example.simulator.controller;
 
+import com.example.simulator.DTOs.*;
+import com.example.simulator.configurations.response.ResponseData;
+import com.example.simulator.configurations.swagger.SwaggerDescription;
+import com.example.simulator.greenhouse.simulators.SimulationType;
+import com.example.simulator.service.GreenhouseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.example.simulator.DTOs.AirConditioningDTO;
-import com.example.simulator.DTOs.Co2DispenserDTO;
-import com.example.simulator.DTOs.LightDTO;
-import com.example.simulator.DTOs.SensorValueDTO;
-import com.example.simulator.DTOs.UpdateHumidifierDTO;
-import com.example.simulator.DTOs.UpdateSalinityDeviceDTO;
-import com.example.simulator.DTOs.WaterDTO;
-import com.example.simulator.configurations.response.ResponseData;
-import com.example.simulator.greenhouse.simulators.SimulationType;
-import com.example.simulator.service.GreenhouseService;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 @RequestMapping("/api/v1/greenhouses/{greenhouseId}")
@@ -36,12 +22,14 @@ public class GreenhouseController {
     @Autowired
     private GreenhouseService greenhouseService;
 
+
+
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201"),
-            @ApiResponse(responseCode = "400", description = "SIMULATION_ALREADY_ADDED"),
-            @ApiResponse(responseCode = "404", description = "GREENHOUSE_DOES_NOT_EXIST")
+            @ApiResponse(responseCode = "201", description = SwaggerDescription.SIMULATION_ALREADY_ADDED),
+            @ApiResponse(responseCode = "400", description = SwaggerDescription.SIMULATION_DOES_NOT_EXIST),
+            @ApiResponse(responseCode = "404", description = SwaggerDescription.GREENHOUSE_DOES_NOT_EXIST)
     })
-    @Operation(summary = "Add the given simulation type at the specified greenhouse")
+    @Operation(summary = SwaggerDescription.ADD_SIMULATION_DESCRIPTION)
     @PostMapping(path = "/addSimulation", produces = "application/json")
     public ResponseEntity<ResponseData> addSimulation(@PathVariable Integer greenhouseId,
                                                       @RequestParam SimulationType type) {
@@ -50,12 +38,10 @@ public class GreenhouseController {
     }
 
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200"),
-            @ApiResponse(responseCode = "400", description = "SIMULATION_OF_GIVEN_TYPE_DOES_NOT_EXIST"),
-            @ApiResponse(responseCode = "404", description = "GREENHOUSE_DOES_NOT_EXIST")
-
+            @ApiResponse(responseCode = "200", description = SwaggerDescription.SIMULATION_DOES_NOT_EXIST),
+            @ApiResponse(responseCode = "404", description = SwaggerDescription.GREENHOUSE_DOES_NOT_EXIST)
     })
-    @Operation(summary = "Delete the given simulation type at the specified greenhouse")
+    @Operation(summary = SwaggerDescription.DELETE_SIMULATION_DESCRIPTION)
     @DeleteMapping(path = "/deleteSimulation", produces = "application/json")
     public ResponseEntity<ResponseData> deleteSimulation(@PathVariable Integer greenhouseId,
                                                          @RequestParam SimulationType type) {
@@ -64,11 +50,10 @@ public class GreenhouseController {
     }
 
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200"),
-            @ApiResponse(responseCode = "400", description = "SIMULATION_OF_GIVEN_TYPE_DOES_NOT_EXIST"),
-            @ApiResponse(responseCode = "404", description = "GREENHOUSE_DOES_NOT_EXIST")
+            @ApiResponse(responseCode = "200", description = SwaggerDescription.SIMULATION_DOES_NOT_EXIST),
+            @ApiResponse(responseCode = "404", description = SwaggerDescription.GREENHOUSE_DOES_NOT_EXIST)
     })
-    @Operation(summary = "Gives the current value of the given simulation")
+    @Operation(summary = SwaggerDescription.GET_SIMULATION_VALUE_DESCRIPTION)
     @GetMapping(path = "/getSimulationValue", produces = "application/json")
     public ResponseEntity<ResponseData<SensorValueDTO>> getSimulationValue(@PathVariable Integer greenhouseId,
                                                                            @RequestParam SimulationType type) {
@@ -77,11 +62,10 @@ public class GreenhouseController {
     }
 
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200"),
-            @ApiResponse(responseCode = "400", description = "SIMULATION_OF_GIVEN_TYPE_DOES_NOT_EXIST"),
-            @ApiResponse(responseCode = "404", description = "GREENHOUSE_DOES_NOT_EXIST")
+            @ApiResponse(responseCode = "200", description = SwaggerDescription.SIMULATION_DOES_NOT_EXIST),
+            @ApiResponse(responseCode = "404", description = SwaggerDescription.GREENHOUSE_DOES_NOT_EXIST)
     })
-    @Operation(summary = "It is used to manage the temperature in the greenhouse")
+    @Operation(summary = SwaggerDescription.SET_AIR_CONDITIONING_DESCRIPTION)
     @PutMapping(path = "/setAirConditioningValue", produces = "application/json")
     public ResponseEntity<ResponseData> setAirConditioningValue(@PathVariable Integer greenhouseId,
                                                                 @Validated @RequestBody AirConditioningDTO airConditioningDTO) {
@@ -90,11 +74,10 @@ public class GreenhouseController {
     }
 
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200"),
-            @ApiResponse(responseCode = "400", description = "SIMULATION_OF_GIVEN_TYPE_DOES_NOT_EXIST"),
-            @ApiResponse(responseCode = "404", description = "GREENHOUSE_DOES_NOT_EXIST")
+            @ApiResponse(responseCode = "200", description = SwaggerDescription.SIMULATION_DOES_NOT_EXIST),
+            @ApiResponse(responseCode = "404", description = SwaggerDescription.GREENHOUSE_DOES_NOT_EXIST)
     })
-    @Operation(summary = "It is used to manage the humidity in the greenhouse")
+    @Operation(summary = SwaggerDescription.SET_HUMIDITY_DESCRIPTION)
     @PutMapping(path = "/setHumidityValue", produces = "application/json")
     public ResponseEntity<ResponseData> setHumidityValue(@PathVariable Integer greenhouseId,
                                                          @Validated @RequestBody UpdateHumidifierDTO dto) {
@@ -103,11 +86,10 @@ public class GreenhouseController {
     }
 
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200"),
-            @ApiResponse(responseCode = "400", description = "SIMULATION_OF_GIVEN_TYPE_DOES_NOT_EXIST"),
-            @ApiResponse(responseCode = "404", description = "GREENHOUSE_DOES_NOT_EXIST")
+            @ApiResponse(responseCode = "200", description = SwaggerDescription.SIMULATION_DOES_NOT_EXIST),
+            @ApiResponse(responseCode = "404", description = SwaggerDescription.GREENHOUSE_DOES_NOT_EXIST)
     })
-    @Operation(summary = "It is used to manage the humidity in the greenhouse")
+    @Operation(summary = SwaggerDescription.SET_WATER_LEVEL_DESCRIPTION)
     @PutMapping(path = "/setWaterLevel", produces = "application/json")
     public ResponseEntity<ResponseData> setWaterLevel(@PathVariable Integer greenhouseId,
                                                       @Validated @RequestBody WaterDTO dto) {
@@ -116,11 +98,10 @@ public class GreenhouseController {
     }
 
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200"),
-            @ApiResponse(responseCode = "400", description = "SIMULATION_OF_GIVEN_TYPE_DOES_NOT_EXIST"),
-            @ApiResponse(responseCode = "404", description = "GREENHOUSE_DOES_NOT_EXIST")
+            @ApiResponse(responseCode = "200", description = SwaggerDescription.SIMULATION_DOES_NOT_EXIST),
+            @ApiResponse(responseCode = "404", description = SwaggerDescription.GREENHOUSE_DOES_NOT_EXIST)
     })
-    @Operation(summary = "It is used to manage the amount of light in the greenhouse")
+    @Operation(summary = SwaggerDescription.SET_LIGHT_DESCRIPTION)
     @PutMapping(path = "/setLight", produces = "application/json")
     public ResponseEntity<ResponseData> setLight(@PathVariable Integer greenhouseId,
                                                  @Validated @RequestBody LightDTO dto) {
@@ -129,11 +110,10 @@ public class GreenhouseController {
     }
 
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200"),
-            @ApiResponse(responseCode = "400", description = "SIMULATION_OF_GIVEN_TYPE_DOES_NOT_EXIST"),
-            @ApiResponse(responseCode = "404", description = "GREENHOUSE_DOES_NOT_EXIST")
+            @ApiResponse(responseCode = "200", description = SwaggerDescription.SIMULATION_DOES_NOT_EXIST),
+            @ApiResponse(responseCode = "404", description = SwaggerDescription.GREENHOUSE_DOES_NOT_EXIST)
     })
-    @Operation(summary = "It is used to manage the amount of CO2 in the greenhouse")
+    @Operation(summary = SwaggerDescription.SET_CO2_DISPENSER_DESCRIPTION)
     @PutMapping(path = "/setCo2Dispenser", produces = "application/json")
     public ResponseEntity<ResponseData> setCo2Dispenser(@PathVariable Integer greenhouseId,
                                                         @Validated @RequestBody Co2DispenserDTO dto) {
@@ -142,15 +122,13 @@ public class GreenhouseController {
     }
 
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200"),
-            @ApiResponse(responseCode = "400", description = "SIMULATION_OF_GIVEN_TYPE_DOES_NOT_EXIST"),
-            @ApiResponse(responseCode = "404", description = "GREENHOUSE_DOES_NOT_EXIST")
+            @ApiResponse(responseCode = "200", description = SwaggerDescription.SIMULATION_DOES_NOT_EXIST),
+            @ApiResponse(responseCode = "404", description = SwaggerDescription.GREENHOUSE_DOES_NOT_EXIST)
     })
-    @Operation(summary = "It is used to manage the salinity of soil")
+    @Operation(summary = SwaggerDescription.SET_SALINITY_DESCRIPTION)
     @PutMapping(path = "/setSalinityValue", produces = "application/json")
     public ResponseEntity<ResponseData> setSalinityValue(@PathVariable Integer greenhouseId, @Validated @RequestBody UpdateSalinityDeviceDTO dto) {
         greenhouseService.setSalinityValue(greenhouseId, dto);
         return ResponseEntity.status(HttpStatus.OK).body(new ResponseData<>());
     }
 }
-
